3f38020f03a1c2135ab8d9f9a4f6c098e7351d08
Carlos Abia <ntt.cabia@iberia.es>
2024-08-08 19:41:58 +0200

    fix game controller

--- src/main/java/com/example/demo/models/Game.java
+++ src/main/java/com/example/demo/models/Game.java
@@ -1,23 +1,16 @@
 package com.example.demo.models;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name = "game", schema = "public")
-
 @Getter
 @Setter
 public class Game {
@@ -25,15 +18,14 @@ public class Game {
 	@Id
 	private String id;
 
-	private String[] cards;
-
+	private List<String> cards;
 
 	public Game() {
+		this.cards = new ArrayList<>();
 	}
 
 	public Game(String id) {
 		this.id = id;
-		this.cards = new ArrayList();
+		this.cards = new ArrayList<>();
 	}
-
 }
